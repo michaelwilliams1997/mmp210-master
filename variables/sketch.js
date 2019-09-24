bdb6d43b373291f7e46cfb4a9c9e91089cd075db
n <n>
2019-09-24 14:24:16 -0400

    Update sketch.js

--- variables/sketch.js
+++ variables/sketch.js
@@ -7,22 +7,22 @@ var y = 200;
 var faceSize = 500;
 
 function setup() {
-    createCanvas(600, 400);
+    createCanvas(610, 420);
 }
 
 function draw() {
     background(100, 0, 255);
     
-    fill("LAVENDER");
+    fill("Blue");
     noStroke();
     ellipse(x, y, faceSize, faceSize/2); // face
     
-    stroke("INDIGO");
+    stroke("red");
     noFill();
     strokeWeight(5);
     fill(250, 100, 100, 100);
    
-    rect(x - 150, y - 100, faceSize/10, faceSize/5); // left eye
+    rect(x - 200, y - 150, faceSize/10, faceSize/5); // left eye
     triangle(
         x + 100, y - 100, 
         x + 150, y - 50, 
@@ -30,9 +30,9 @@ function draw() {
     ); // right eye
     
     strokeWeight(10);
-    stroke("#00ffff");
+    stroke("black");
     line(
-        x - 100, y + 100, 
+        x - 150, y + 150, 
         x + 100, y + 150
     ); // mouth
 }
